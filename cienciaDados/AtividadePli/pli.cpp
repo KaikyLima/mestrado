6ac0f4a51594605f0dcecfd8e6a191b3b6a204c4
Kaiky Lima <kslima@coamo.com.br>
2026-04-27 14:37:00 -0300

    PLI string

--- cienciaDados/AtividadePli/pli.cpp
+++ cienciaDados/AtividadePli/pli.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <unordered_map>
 #include <vector>
+#include <string>
 
 using namespace std;
 
-void imprimir_pli(char nome, int coluna[], int tamanho) {
-    unordered_map<int, vector<int>> grupos;
+void imprimir_pli(char nome, string coluna[], int tamanho) {
+    unordered_map<string, vector<int>> grupos;
     for (int i = 0; i < tamanho; i++) {
         grupos[coluna[i]].push_back(i);
     }
@@ -30,9 +31,10 @@ void imprimir_pli(char nome, int coluna[], int tamanho) {
 
 int main() {
     int tamanho = 5;
-    int colunaA[tamanho] = {1, 2, 1, 4, 1};
-    int colunaB[tamanho] = {9, 8, 9, 6, 8};
-    int colunaC[tamanho] = {3, 3, 4, 3, 4};
+    string colunaA[tamanho] = {"1", "2", "1", "4", "1"};
+    string colunaB[tamanho] = {"9", "8", "9", "6", "8"};
+    string colunaC[tamanho] = {"3", "3", "4", "3", "4"};
+    string colunaD[tamanho] = {"0", "0", "1", "1", "0"};
 
     cout << "--- Construindo as PLIs ---\n\n";
 
@@ -41,6 +43,8 @@ int main() {
     imprimir_pli('B', colunaB, tamanho);
 
     imprimir_pli('C', colunaC, tamanho);
+
+    imprimir_pli('D', colunaD, tamanho);
 
     return 0;
 }
