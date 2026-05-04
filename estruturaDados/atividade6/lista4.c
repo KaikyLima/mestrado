5cc15e4fa9dc4e40c20eecabcf9a256b4157b2d6
Kaiky Lima <kslima@coamo.com.br>
2026-05-04 08:06:36 -0300

    lista encadeado

--- estruturaDados/atividade6/lista4.c
+++ estruturaDados/atividade6/lista4.c
@@ -9,65 +9,40 @@ void inicializar(Lista* l){
 
 }
 
-int inserirOrdenado(Lista* l, ELEMENTO e){
-
-    No *novo = (No*)malloc(sizeof(No)); 
+int inserirOrdenado(Lista* l, ELEMENTO e) {
+    No* novo = (No*)malloc(sizeof(No));
     if (novo == NULL) return 0;
     novo->dado = e;
     novo->proximo = NULL;
+
     int contador = 0;
 
-
     if (l->inicio == NULL || e < l->inicio->dado) {
-        if (l->inicio != NULL) contador++;
         novo->proximo = l->inicio;
-
-        if (l->tamanho == 0)
-        {
-            l->fim = novo;
-        }
+        l->inicio = novo;
+        if (l->tamanho == 0) l->fim = novo;
         l->tamanho++;
-        return contador;
+        return 0;
     }
-    
-    contador++;
-    if (l->fim->dado <= e)
-    {
+    contador++; 
+    if (e >= l->fim->dado) {
         l->fim->proximo = novo;
         l->fim = novo;
         l->tamanho++;
         return contador;
     }
 
-
-    else {
-        No *aux1;
-        No *aux2;
-        aux1 = l->inicio;
-        aux2 = l->inicio->proximo;
-        
-        for(int i = 0; i > l->tamanho ; i++ ){
-            if (aux1->dado < e && aux2->dado <= e){
-                aux1 = aux2;
-                aux2 = aux2->proximo;
-                contador++;
-            }
-            else
-            {
-                aux1->proximo = novo;
-                novo->proximo = aux2;
-                contador++;
-
-            }
-            
-        }
-
-        free(aux1);
-        free(aux2);
-
+    No* atual = l->inicio;
+    while (atual->proximo != NULL && atual->proximo->dado < e) {
+        contador++;
+        atual = atual->proximo;
     }
+    contador++; 
+
+    novo->proximo = atual->proximo;
+    atual->proximo = novo;
     l->tamanho++;
-    free(novo);
+
     return contador;
 }
 
@@ -83,18 +58,13 @@ void destruir(Lista* l) {
     l->tamanho = 0;
 }
 
-void imprimirInverso(Lista* l){
-    if (l->tamanho == 0) return;
-    No *aux;
-    aux = l->inicio;
-    
-    ELEMENTO inverso[l->tamanho];
-    for(int i =0; i < l->tamanho; i++){
-        inverso[i] = aux->dado;
-        aux=aux->proximo;
-    }
-    for (int i = l->tamanho -1 ; i >=0 ; i--){
-        printf("\n %d", inverso[i]);
-    }
-    free(aux);
+void imprimirRecursivo(No* n) {
+    if (n == NULL) return;
+    imprimirRecursivo(n->proximo);
+    printf("%d ", n->dado);
+}
+
+void imprimirInverso(Lista* l) {
+    if (l->inicio == NULL) return;
+    imprimirRecursivo(l->inicio);
 }
