ccabd99bbc0af0e7de4458f0f280ce403c373a64
Kaiky Lima <kslima@coamo.com.br>
2026-05-06 10:24:46 -0300

    install jenga

--- cienciaDados/atv2/setup_env.ps1
+++ cienciaDados/atv2/setup_env.ps1
@@ -5,6 +5,6 @@ pip install --upgrade pip
 pip install setuptools==65.5.0
 pip install wheel
 
-pip install -r requirements.txt
+pip install -r requirements-Jenga.txt
 
 pip install git+https://github.com/schelterlabs/jenga.git --no-build-isolation
